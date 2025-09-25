9eede303ba683972c5c1cb8d6577ab1299985bb2
ivanmorata <ivan.morata@cifpceuta.es>
2025-09-25 19:12:29 +0200

    v1.1 -Añadido el ejercicio 8 del tema 1.

--- src/Tema1/Ejercicio7.java
+++ src/Tema1/Ejercicio7.java
@@ -13,4 +13,3 @@ public class Ejercicio7 {
         }
     }
 }
-
---
+++ src/Tema1/Ejercicio8.java
@@ -0,0 +1,17 @@
+package Tema1;
+
+import java.io.IOException;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+
+        ProcessBuilder pb = new ProcessBuilder("ping", "-c", "4", "google.com"); //Paso 2.1: Para más de un comando, separar por comas.
+        pb.inheritIO(); //Paso 2.2: Redirigir los flujos automáticamente
+        try {
+            Process process = pb.start();
+            process.waitFor(); //Paso 4*: cuando el proceso tiene una duración de más de un instante, hay que esperar a que termine.
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
