3cf613097f0f126fcc36a160dca9546f1c1f9430
ivanmorata <ivan.morata@cifpceuta.es>
2025-09-25 18:52:38 +0200

    v1.0 -Creación del repositorio PSP2DAM_25-26 -Añadido el ejercicio 7 del tema 1.

---
+++ src/Tema1/Ejercicio7.java
@@ -0,0 +1,16 @@
+package Tema1;
+
+import java.io.IOException;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        String comando = "gnome-calculator"; //Paso 1: Guardar el comando en un String
+        ProcessBuilder pb = new ProcessBuilder(comando); //Paso 2: Crear objeto de ProcessBuilder.
+        try {
+            Process proceso = pb.start(); //Paso 3: Crear objeto Process y utilizar start()
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
+
